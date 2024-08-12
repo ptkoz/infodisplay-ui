d2d35e4858ff5f4bdfa3035d80a64bd17643c163
Patryk Kozlowski <9805525+ptkoz@users.noreply.github.com>
2024-08-12 18:12:16 +0200

    Adjusted displayed night hours

--- src/sections/Settings.tsx
+++ src/sections/Settings.tsx
@@ -17,8 +17,8 @@ import { MeasureKind } from "../store/Measures/types.ts";
 import { DeviceKind, DeviceSettings, OperatingMode } from "../store/Device/types.ts";
 import { sendToBackend } from "../store/BackendSynchronization.ts";
 
-const DAY_HOURS = "06:00 - 22:59";
-const NIGHT_HOURS = "23:00 - 05:59";
+const DAY_HOURS = "06:00 - 21:59";
+const NIGHT_HOURS = :00 - 05:59";
 
 type ControllingStatus = DeviceSettings["controlledBy"];
 
